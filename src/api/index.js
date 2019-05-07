92efd5a034198e5242acbdeede943adf7efc4849
zhangchenwei <1062270453@qq.com>
2019-05-07 19:40:16 +0800

    update

--- src/api/index.js
+++ src/api/index.js
@@ -4,13 +4,27 @@ import ajax from './ajax'
 
 const BASE = '/api'
 
-//1、根据经纬度获取位置详情
+//根据经纬度获取位置详情
 export const reqAddress = (latitude,longitude)=>ajax(BASE + `/position/${latitude},${longitude}`)
 
-//2、获取食品分类列表
+//获取食品分类列表
 export const reqCategorys = ()=>ajax(BASE + `/index_category`)
 
-//3、根据经纬度获取商铺列表
+//根据经纬度获取商铺列表
 export const reqShops = ({latitude,longitude})=>ajax(BASE + `/shops`,{latitude,longitude})
 
-//
+//用户名密码登陆
+export const reqPwdLogin = ({name,pwd,captcha})=>ajax(BASE + '/login_pwd',{name,pwd,captcha},'POST')
+
+//发送短信验证码
+export const reqCode = (phone)=>ajax(BASE + '/sendcode',{phone})
+
+//手机号验证码登陆
+export const reqSmsLogin = (phone,code)=>ajax(BASE + '/login_sms',{phone,code},'POST')
+
+//根据会话获取用户信息
+export const reqUser = ()=>ajax(BASE + '/userinfo')
+
+//退出登录
+export const logout = ()=>ajax(BASE + '/logout')
+
